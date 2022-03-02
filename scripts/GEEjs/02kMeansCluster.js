9cb7737faf389b10e06ac2a0633a51258fb92e19
Peter C <clarype@oregonstate.edu>
2022-03-02 09:13:41 -0800

    Update 02kMeansCluster.js

--- scripts/GEEjs/02kMeansCluster.js
+++ scripts/GEEjs/02kMeansCluster.js
@@ -40,7 +40,7 @@ var ltgee = require('users/emaprlab/public:Modules/LandTrendr.js');
 /////////////////point sample ////////////////////////////
 ///////////////////////////////////////////////////////
 
-var sample = ee.FeatureCollection("users/clarype/ltop_snic_seed_polygon_centroids_att_sample_75k_gdal");
+var sample = ee.FeatureCollection("users/clarype/ltop_snic_seed_polygons_centroids_att_sample_75k_gdal");
 Map.addLayer(sample)
 //////////////////////////////////////////////////////////
 /////////////////////Cambodia vector////////////////////////////
@@ -98,7 +98,7 @@ var SNIC_means_image = patchRepSeeds.multiply(patchRepsMean).select(["B1_mean", 
 
 var training = ee.Clusterer.wekaCascadeKMeans(5000,5001).train({ 
   features: sample, 
-  //inputProperties:["B1_mean", "B2_mean",  "B3_mean",  "B4_mean",  "B5_mean",  "B7_mean",  "B1_1_mean",  "B2_1_mean",  "B3_1_mean",  "B4_1_mean",  "B5_1_mean","B7_1_mean",  "B1_2_mean",  "B2_2_mean",  "B3_2_mean",  "B4_2_mean",  "B5_2_mean",  "B7_2_mean"]
+  //real names:["B1_mean", "B2_mean",  "B3_mean",  "B4_mean",  "B5_mean",  "B7_mean",  "B1_1_mean",  "B2_1_mean",  "B3_1_mean",  "B4_1_mean",  "B5_1_mean","B7_1_mean",  "B1_2_mean",  "B2_2_mean",  "B3_2_mean",  "B4_2_mean",  "B5_2_mean",  "B7_2_mean"]
   inputProperties:["seed_3",  "seed_4",  "seed_5",  "seed_6",  "seed_7",  "seed_8",  "seed_9",  "seed_10",  "seed_11",  "seed_12","seed_13",  "seed_14",  "seed_15",  "seed_16",  "seed_17",  "seed_18",  "seed_19", "seed_20"]
 });
 
